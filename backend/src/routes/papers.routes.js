0b3ba2d68fc5448ac849dd32d25e1ae33da16864
Shrutiii <154013843+ladyvengean@users.noreply.github.com>
2025-04-18 05:33:07 +0530

    made more

--- backend/src/routes/papers.routes.js
+++ backend/src/routes/papers.routes.js
@@ -3,18 +3,18 @@ import axios from "axios";
 
 const router = express.Router();
 
-// Notice the route path is just "/" instead of "/papers" since you're already mounting at "/api/v1/papers"
+
 router.get("/", async (req, res) => {
   try {
-    // Extract query parameters from the user's request
+    
     const { topic = "ai", max_results = 5, start = 0 } = req.query;
     
-    // Fetch papers from arXiv based on query parameters
+    
     const response = await axios.get(
       `http://export.arxiv.org/api/query?search_query=all:${topic}&start=${start}&max_results=${max_results}`
     );
     
-    // Send the raw XML response back
+   
     res.send(response.data);
   } catch (err) {
     console.error(err);
